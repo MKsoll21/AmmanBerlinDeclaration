edd19c00e851c4ef2d00050d480f118b986968e3
MKsoll21 <moritz.ksoll@web.de>
2026-07-28 15:03:09 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -158,6 +158,16 @@ data["Donor"] = (
     .replace(name_mapping)
 )
 
+# ---------------------------------------------------
+# Exclude unspecified recipients
+# ---------------------------------------------------
+
+data = data[
+    ~data["Recipient"]
+    .astype(str)
+    .str.contains("unspecified", case=False, na=False)
+]
+
 
 
 
